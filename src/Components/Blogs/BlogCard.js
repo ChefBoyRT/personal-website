2a734e048f9f1d3fbf733fdf02c414adbb132e57
Taylor <stein0209@gmail.com>
2019-10-01 16:10:41 -0600

    adds updates blog card styling and hover action for links

--- src/Components/Blogs/BlogCard.js
+++ src/Components/Blogs/BlogCard.js
@@ -2,15 +2,19 @@ import React from 'react'
 
 export default function BlogCard({ blog }) {
     return (
-        <div className='blog-card' onClick={() => window.open(`${blog.link}`, "_blank")}>
-            <div className='blog-header'>
-                <img className='blog-image' src={blog.image_url} alt={blog.title}/>
+        <>
+            <div className='blog-card' onClick={() => window.open(`${blog.link}`, "_blank")}>
+                <div className='blog-header'>
+                    <img className='blog-image' src={blog.image_url} alt={blog.title}/>
+                </div>
+                <div className='blog-info'>
+                    <h3 className='blog-title'>{blog.title}</h3>
+                    <p className='blog-date-time'>Published {blog.publication_date} <span className='blog-pipe'>|</span> Read Time {blog.read_time} min.</p>
+                    <p className='blog-description'>{blog.description}</p>
+                </div>
             </div>
-            <div className='blog-info'>
-                <h3 className='blog-title'>{blog.title}</h3>
-                <p className='blog-date-time'>Published {blog.publication_date} | Read Time {blog.read_time}</p>
-                <p className='blog-description'>{blog.description}</p>
-            </div>
-        </div>
+        </>
     )
 }
+
+
---
+++ src/Components/Blogs/BlogHeader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BlogHeader() {
+    return (
+        <>
+          <h1 className='blog-component-header'>Blogs</h1>  
+        </>
+    )
+}
