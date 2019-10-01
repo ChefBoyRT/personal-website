0a8fdbe8f0b90ebf7911a4d54f38b4f22873bbf8
Taylor <stein0209@gmail.com>
2019-10-01 10:33:54 -0600

    fixes about and contact formating with flex

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -9,9 +9,10 @@ const Contact = () => {
                     <h1 className='contact-header'>Contact</h1>
                     <img className='contact-icon' src='https://image.flaticon.com/icons/svg/1157/1157044.svg' alt='contact icon' />
                     <div className='contact-card-info'>
-                        <p><span className='contact'>Phone Number:</span> 303.910.1898</p>
-                        <p><span className='contact'>Email Address:</span> taylor.v.stein@gmail.com</p>
-                        <p><span className='contact'>LinkedIn:</span> taylor.v.stein@gmail.com</p>
+                        <p><span className='contact'>Email: </span> <a className='about-email' href="mailto:taylor.v.stein@gmail.com">taylor.v.stein@gmail.com</a></p>
+                        <p><span className='contact'>Phone: </span><a className='about-phone' href="tel:303.910.1898">303.910.1898</a></p>
+                        <img onClick={()=> window.open("https://www.linkedin.com/in/taylor-stein/", "_blank")} className='linkedin-logo' src='https://image.flaticon.com/icons/svg/145/145807.svg' alt='linkedin logo' />
+                        <img onClick={()=> window.open("https://github.com/ChefBoyRT", "_blank")} className='github-logo' src='https://image.flaticon.com/icons/svg/179/179323.svg' alt='github-logo' />
                     </div>
                 </div>
             </div>
