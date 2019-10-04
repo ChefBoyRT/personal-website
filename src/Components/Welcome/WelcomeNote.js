45a826d6cf8e6b2ad59b6683567b1a86eed65ddb
Taylor <stein0209@gmail.com>
2019-10-03 19:51:48 -0600

    fixes about

--- src/Components/Welcome/WelcomeNote.js
+++ src/Components/Welcome/WelcomeNote.js
@@ -12,8 +12,9 @@ export default function WelcomeNote() {
                     <div className='welcome-note-content-container'>
                         <p className='welcome-note-text'>
                             Welcome to my website and thank you so much for taking the time to review my work. 
-                            Software development fascinates me. Over the last 15 weeks, I have had the privilege
-                            to work with some of the most brilliant people and be insipired by the incredible
+                            Software development fascinates me. Before emabarking on my software development journey,
+                            I graduated with an MBA at University of Denver. Since then, I have had the privilege
+                            of working with some of the most brilliant people and be insipired by the incredible
                             instructors at the Flatiron School. This website is a complete representation of myself
                             and everything I have to offer, for now, more to come. Do not hesitate to reach out to me through
                             my contact page if you would like to connect or have any further questions. Thanks again for
